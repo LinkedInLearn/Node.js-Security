38ad28eef7f1882601496378a767a55cecb93f3d
Help <pczerokey@gmail.com>
2020-07-30 17:02:34 -0400

    installed helmet dependency for security

--- Project/src/controllers/crmController.js
+++ Project/src/controllers/crmController.js
@@ -6,6 +6,8 @@ const Contact = mongoose.model('Contact', ContactSchema);
 export const addNewContact = (req, res) => {
     let newContact = new Contact(req.body);
 
+
+
     newContact.save((err, contact) => {
         if (err) {
             res.send(err);
