9cbe3d723acda4032334f4bcb7a8dc5e52e53779
Help <pczerokey@gmail.com>
2020-07-30 17:46:34 -0400

    fix rate limited on calls

--- Project/index.js
+++ Project/index.js
@@ -3,18 +3,19 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import routes from './src/routes/crmRoutes';
 import helmet from "helmet";
-import RateLimit from "express-rate-limit";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 const PORT = 3000;
 
 app.use(helmet())
 
-const limiter = new RateLimit=({
+const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, //15 minutes
     max: 100, // limit of request per ip
     delayMs: 0 //no delay
 })
+app.use(limiter)
 
 // mongoose connection
 mongoose.Promise = global.Promise;
